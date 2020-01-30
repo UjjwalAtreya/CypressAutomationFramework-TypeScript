c470fb86a0cc7710423242eeb08ef06041079156
Ujjwal <ujjwal.atreya@rizepoint.com>
2020-01-30 09:12:52 +0545

    Test circleCi

---
+++ cypress/integration/AllTests/googleSearchTest1.spec.ts
@@ -0,0 +1,17 @@
+import { google } from "../PageObjects/googleSearch"
+describe("This is sample test suit",function(){
+   
+    before(function(){
+        cy.fixture('testData').then(function(data){
+            this.dataObj=data
+        })
+    })
+
+     it('should write in search box',function(){
+     cy.visit(this.dataObj.baseUrl)
+     google.typeInSearchBox(this.dataObj.searchText)
+     google.verifyIfTextExists(this.dataObj.expectedSearchSelector,this.dataObj.searchText)
+
+    })
+
+})
